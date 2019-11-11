96e826386b08ac8aa2884e9186ff34f053c8ea5b
aurelien <a.delval87@gmail.com>
2019-11-11 15:08:56 +0100

    Remove benchmark executable, +gitignore

--- Ultimate-Community-Finder/benchmarks.cpp
+++ Ultimate-Community-Finder/benchmarks.cpp
@@ -9,7 +9,7 @@ class random:public::hayai::Fixture {
 public:
     virtual void SetUp() {
         this->graph = new Graph();
-        this->graph->random(250, 0.2);
+        this->graph->random(100, 0.2);
     }
 
     virtual void TearDown() {
@@ -24,7 +24,7 @@ class barabasiAlbert:public::hayai::Fixture {
 public:
     virtual void SetUp() {
         this->graph = new Graph();
-        this->graph->barabasiAlbert(250, 4, 5);
+        this->graph->barabasiAlbert(100, 4, 5);
     }
 
     virtual void TearDown() {
@@ -38,44 +38,44 @@ public:
 
 //bronKerbosch
 
-BENCHMARK_F(random, bronKerboschRandom, 5, 1) {
+BENCHMARK_F(random, bronKerbosch, 50, 1) {
     bronKerbosch(*graph);
 }
 
-BENCHMARK_F(barabasiAlbert, bronKerboschBarabasiAlbert, 5, 1) {
+BENCHMARK_F(barabasiAlbert, bronKerbosch, 50, 1) {
     bronKerbosch(*graph);
 }
 
 
 //bronKerboschOrdering
 
-BENCHMARK_F(random, bronKerboschOrderingRandom, 5, 1) {
+BENCHMARK_F(random, bronKerboschOrdering, 50, 1) {
     bronKerboschOrdering(*graph);
 }
 
-BENCHMARK_F(barabasiAlbert, bronKerboschOrderingBarabasiAlbert, 5, 1) {
+BENCHMARK_F(barabasiAlbert, bronKerboschOrdering, 50, 1) {
     bronKerboschOrdering(*graph);
 }
 
 
 //maxCliques1
 
-BENCHMARK_F(random, maxCliques1Random, 5, 1) {
+BENCHMARK_F(random, maxCliques1, 50, 1) {
     maxCliques1(*graph);
 }
 
-BENCHMARK_F(barabasiAlbert, maxCliques1BarabasiAlbert, 5, 1) {
+BENCHMARK_F(barabasiAlbert, maxCliques1, 50, 1) {
     maxCliques1(*graph);
 }
 
 
 //maxCliques2
 
-BENCHMARK_F(random, maxCliques2Random, 5, 1) {
+BENCHMARK_F(random, maxCliques2, 50, 1) {
     maxCliques2(*graph);
 }
 
-BENCHMARK_F(barabasiAlbert, maxCliques2BarabasiAlbert, 5, 1) {
+BENCHMARK_F(barabasiAlbert, maxCliques2, 50, 1) {
     maxCliques2(*graph);
 }
 
